437741a2bc23ff8630b7917e182a64cc6576f6c0
zhangshuaiAA9 <2448957229@qq.com>
2024-09-10 21:43:51 +0800

    hello

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vtop.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+int main() 
+{
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+  
+  while(1)
+  	{
+  		dut.eval();
+  		nvboard_update();
+  	}
   return 0;
 }
